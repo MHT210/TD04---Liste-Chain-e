998f8819a9f98ac8b6f12439732228757711a709
MHT210 <mehmetozler0812@gmail.com>
2025-10-26 11:39:46 +0100

    Ajout de la fonction recherche

--- struct.c
+++ struct.c
@@ -22,29 +22,25 @@ LISTE_POINTS * insert_first(LISTE_POINTS *list, int x, int y) {
 }
 
 LISTE_POINTS * insert_last(LISTE_POINTS *list, int x, int y) {
-	LISTE_POINTS * temp = list;
-	LISTE_POINTS * previous_temp;
-	LISTE_POINTS * last;
-	while (temp) {
-		previous_temp = temp;
-		temp = (*temp).suiv;
+	if (!list) {
+		LISTE_POINTS * last = malloc(sizeof(LISTE_POINTS));
+		if (!last) {fprintf(stderr, "pb malloc\n"); exit(12);}
+		(*last).p.x = x;
+		(*last).p.y = y;
+		(*last).suiv = NULL;
+		return last;
 	}
-
-	last = malloc(sizeof(LISTE_POINTS));
-	if (!last) {fprintf(stderr, "pb malloc\n"); exit(12);}
 	
-	(*last).p.x = x;
-	(*last).p.y = y;
-	temp = (*last).suiv;
-	(*temp).suiv = last;
-
-	return last;
+	list->suiv = insert_last(list->suiv, x, y);
+	return list;
 }
 
 int count_values(LISTE_POINTS *list) {
 	int i = 0;
-	if (list->p.x) {i++;}
-	if (list->p.y) {i++;}
+	while (list) {
+		i++;
+		list = list->suiv;
+	}
 	return i;
 }
 
@@ -57,14 +53,34 @@ void print_list(LISTE_POINTS *list) {
 	printf(" // \n");
 }
 
+int research(LISTE_POINTS *list, int x, int y) {
+	int i = 0;
+	while (list) {
+		i++;
+		if (list->p.x == x && list->p.y == y) {return i;}
+		list = list->suiv;
+	}
+
+	return 0;
+}
+
+void research_rec(LISTE_POINTS *list, int x, int y) {
+	if (list->p.x == x && list->p.y == y) {printf("Found : [%d, %d] \n", x, y);}
+	if (list->suiv) {research_rec(list->suiv, x, y);}
+}
+
 int main () {
 	LISTE_POINTS *l;
 	l = NULL;
 	l = insert_first(l, 1, 2);
 	l = insert_first(l, 3, 4);
 	print_list(l);
+	
 	l = insert_last(l, 1, 1);
 	print_list(l);
+	
+	printf("L'element est a la pos : %d \n",research(l, 1, 2));
+	research_rec(l, 1, 1);
 
 	return 0;
 }
